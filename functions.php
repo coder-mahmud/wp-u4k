6376bb9f08adc574a1bfae42bba50a97215be220
Mahmud <mahmud.linked@gmail.com>
2018-10-21 17:50:13 +0600

    First commit after theme setup

---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+
+
+/* Theme styles and JS */
+wp_enqueue_script('jquery');
+function cwp_fc_scripts(){
+  
+
+  	wp_enqueue_style('normalize-style', get_template_directory_uri() .'/css/normalize.css');
+  	wp_enqueue_style('bootstrap-style', get_template_directory_uri() .'/css/bootstrap.min.css');
+  	wp_enqueue_style('flexnav', get_template_directory_uri() .'/css/flexnav.css');
+
+  	wp_enqueue_style('main-style', get_template_directory_uri() .'/css/main.css');
+ 	wp_enqueue_style( 'bxslider', 'https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css' );
+  	wp_enqueue_style( 'kanit-font', 'https://fonts.googleapis.com/css?family=Kanit:400,500,700,800,900' );
+	wp_enqueue_style( 'style', get_stylesheet_uri(),NULL, microtime() );
+	wp_enqueue_style('lessframework-style', get_template_directory_uri() .'/css/lessframework.css',NULL, microtime() );
+        
+  	wp_deregister_script('jquery');
+  	wp_enqueue_script('modernizer_script', get_template_directory_uri() .'/js/vendor/modernizr-3.5.0.min.js',array(),'',true);
+
+	wp_enqueue_script('bx-script', 'https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js',array(),'',true);
+  	wp_enqueue_script('bootstrap-popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', array(), null, true);
+  	wp_enqueue_script('bootstrap', get_template_directory_uri() .'/js/bootstrap.min.js',array(),'',true);
+   	wp_enqueue_script('main_script', get_template_directory_uri() .'/js/main.js',array(),microtime(),true);
+   	wp_enqueue_script('main_script', get_template_directory_uri() .'/js/jquery.flexnav.js',array(),'',true);
+   	wp_enqueue_script('jq','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js',array(),'',false);
+
+}
+
+
+//add_action('wp_enqueue_scripts','cwp_fc_scripts');
+
+
+
+
+/*   Script tag for 3rd party JS resources */
+function add_id_to_script( $tag, $handle, $src ) {
+
+    if ( 'bootstrap-popper' === $handle ){
+        $tag = '<script type="text/javascript" src="' . $src . '"  integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script> ';
+    }
+
+    return $tag;
+}
+
+
+//add_filter( 'script_loader_tag', 'add_id_to_script', 10, 3 );
---
+++ index.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php wp_title(); ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css">
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/flexnav.css" media="screen, projection" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/lessframework.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Kanit:400,500,700,800,900" rel="stylesheet">
+
+        <?php wp_head(); ?>   
+    </head>
+
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    	<div class="header_area">
+            <div class="">
+                <div class="row ">
+                    <div class="col-md-12 align-self-center">
+                        <div class="logo_menu_button_holder">
+                            <a class="logo" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt=""></a>
+                                <div class="menu-button">
+                                    <div id="nav-icon3">
+                                      <span></span>
+                                      <span></span>
+                                      <span></span>
+                                      <span></span>
+                                    </div>                            
+
+                                </div>
+                           
+
+                        </div>
+
+                        <div class="menu_holder">
+                            <ul data-breakpoint="768" class="flexnav">
+                                <li><a class="hvr-underline-from-center" href="#">Our Partners</a> </li>
+                                <li><a class="hvr-underline-from-center" href="#">Who we are</a></li>
+                                <li><a class="hvr-underline-from-center" href="#">Media & Events</a></li>
+                                <li><a class="hvr-underline-from-center" href="#">Contact</a></li>
+                                <li><a class="hvr-underline-from-center" href="#">Members Area</a></li>
+                                <li><a class="hvr-underline-from-center" href="#">Donate</a></li>
+                            </ul>                
+                        </div>           
+
+                </div>
+            </div>
+    	
+    	</div>
+    </div>
+
+		
+        <div class="home_hero_area">
+            <div class="custom_container">
+                <div class="row align-items-center">
+
+                    <div class="col-md-6 pad_0_10 m_m_b_15">
+                        <div class="text_section">
+                            <h2 class="blue_text_with_yellow_bg black_900">Transforming</h2>
+                            <h2 class="blue_text_with_yellow_bg black_900">unwanted uniforms </h2>
+                            <h2 class="blue_text_with_yellow_bg black_900">into clothes for </h2>
+                            <h2 class="blue_text_with_yellow_bg black_900">children in need.</h2>
+                            <p class="muse_font m_t_25">Through uniforms 4 Kids we are closing the gap between those serving in uniform, our communities and children. The program sees the uniforms continue to protect and serve. </p>
+                            <a href="#" class="yellow_box_shadow hvr-fade fw_700">Donate Now</a>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>      
+
+       
+
+        <div class="blog_area featured_blog_area">
+            <div class="container">
+                <div class="row">
+
+                    
+                    <div class="col-md-4">
+                        
+                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
+                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
+                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
+                        <p class="muse_font">provided to children in need</p>
+                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        
+                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
+                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
+                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
+                        <p class="muse_font">provided to children in need</p>
+                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        
+                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
+                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
+                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
+                        <p class="muse_font">provided to children in need</p>
+                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
+                    </div>
+
+                    
+
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="testimonial_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Why we do it</h2>
+
+                        <div class="slider">
+
+                            <div>
+
+                                <div class="text_holder ">
+                                    <p class="testimonial_text">Since getting this new clothes, I no longer worry about getting bullied at school. I feel like I can finally fit in!</p>
+                                    <p class="testi_client_name">Anthony, 7 years old,Capalaba Queensland</p>
+
+                                    <div class="testimonial_image_holder"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cropped.png" alt=""></div>
+
+                                </div>
+
+                            </div>
+
+                            <div>
+
+                                <div class="text_holder">
+                                    <p class="testimonial_text">Since getting this new clothes, I no longer worry about getting bullied at school. I feel like I can finally fit in!</p>
+                                    <p class="testi_client_name">Anthony, 7 years old,Capalaba Queensland</p>
+
+                                    <div class="testimonial_image_holder"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cropped.png" alt=""></div>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="email_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-4">
+                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Stay up to date</h2>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-8">
+                        <form action="">
+                            <input class="email" type="email" placeholder="Email Address">
+                            <input type="submit" value="Subscribe" class="white_box_shadow hvr_fade_yellow fw_700 fz_25 ">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="footer_area">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6">
+                        <img class="footer_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer_logo.png" alt="">
+                    </div>
+                    <div class="col-md-3">
+                        <h3 class="white_text_gray_bg fw_900 fz_35 lh_20 footer_column_heading">Follow us</h3>
+                        <ul class="footer_links">
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Twitter</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h3 class="white_text_gray_bg fw_900 fz_35 lh_20 footer_column_heading">Support us</h3>
+                        <ul class="footer_links">
+                            <li><a href="#">Donate</a></li>
+                            <li><a href="#">Partner with us</a></li>
+                            <li><a href="#">Get Involved</a></li>
+                        </ul>                        
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="footer_bottom_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <hr class="gray_line">
+                        <p class="muse_font">&copy; Uniform 4 Kids 2018</p>                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>       
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
+
+
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.flexnav.js" type="text/javascript"></script>
+
+	
+        <script type="text/javascript">
+        
+
+            jQuery(document).ready(function($) {
+                
+                // initialize FlexNav
+                /*
+                $(".flexnav").flexNav({
+                    'transitionSpeed' : '1.6s', 'itemHeight': 50,
+                    'animationSpeed':     650, 
+                });
+                */
+
+
+                $(".flexnav").flexNav({
+                    'animationSpeed' : 1000
+                });
+
+                $('.slider').bxSlider();
+
+                $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+                    $(this).toggleClass('open');
+                });
+                console.log('ki khobor!!');
+            });
+        
+        </script>	
+		
+		<?php wp_footer(); ?>
+    </body>
+</html>
