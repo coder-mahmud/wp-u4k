9d216e90a34ebd5dd9dc69cc74cf311fbb134bef
Mahmud <mahmud.linked@gmail.com>
2018-10-22 09:48:37 +0600

    After contact page

---
+++ template-contact.php
@@ -0,0 +1,50 @@
+<?php 
+
+/*
+Template Name: Contact
+*/
+get_header(); ?>
+
+		
+        <div class="home_hero_area hero_area contact_hero">
+            <div class="custom_container">
+                <div class="row align-items-center">
+
+                    <div class="col-md-6 m_m_b_15">
+                        <div class="text_section">
+                            <h2 class="blue_text_with_yellow_bg black_900">Contact Us</h2>
+                            
+                            <p class="muse_font">For all general inquires email </p>
+                            <p class="muse_font"><a href="mailto:info@uniforms4kids.org.au">info@uniforms4kids.org.au</a> </p>
+                            
+                            <div class="address_box">
+                                <p class="name fw_700">Anne Macdonald APM</p>
+                                <p class="muse_font">Coordinator</p>
+                                <p class="muse_font">P: 0400 000 000</p>
+                                <p class="muse_font"><a href="mailto:annie@uniforms4kids.com.au">E: annie@uniforms4kids.com.au</a></p>
+
+                            </div>
+
+                            <div class="address_box">
+                                <p class="name fw_700">Pauline Pattinson</p>
+                                <p class="muse_font">Media & Communications</p>
+                                <p class="muse_font">P: 0400 000 000</p>
+                                <p class="muse_font"><a href="mailto:pauline@uniforms4kids.com.au">E: pauline@uniforms4kids.com.au</a></p>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 no_padding_on_mobile">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ template-single.php
@@ -0,0 +1,70 @@
+<?php 
+
+/*
+Template Name: single template
+*/
+get_header(); ?>
+
+		
+        <div class="home_hero_area hero_area single_event_hero_area">
+            <div class="custom_container">
+                <div class="row align-items-center">
+
+                    <div class="col-md-6 m_m_b_15">
+                        <div class="text_section">
+                            <h2 class="red_text_yellow_bg fz_45 black_900">Hero news</h2>
+                            <h2 class="red_text_yellow_bg fz_45 black_900">articles go in </h2>
+                            <h2 class="red_text_yellow_bg fz_45 black_900">this main</h2>
+                            <h2 class="red_text_yellow_bg fz_45 black_900">section up here.</h2>
+                            <p class="muse_font m_t_25">Without our people , we'd be nothing. With a diverse range of skillset and experience we're a team who are passionate about making a real difference in the world and we always do it with a smile.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 no_padding_on_mobile">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>      
+
+       <div class="single_event_content">
+           <div class="row">
+               <div class="col-md-12">
+                   <a href="#" class="back_media left_aligned">Back to Media & Events</a>
+                   <a href="#" class="back_media right_aligned">Next Article</a>
+               </div>
+
+               <div class="col-md-12 limited_width_single_content">
+                   
+
+               </div>
+           </div>
+       </div>
+
+
+
+
+
+
+        <div class="get_involved_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 footer_hero">
+                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Donate now</h2>
+                        <p class="muse_font fz_25">Are you passionate about our cause and want to get involved? Contact us to see what ways you can volunteer and help change children's lives. </p>
+                        <a href="#" class="white_box_shadow hvr_fade_yellow fw_700 fz_25 ">Contact Us</a>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+<?php get_footer(); ?>
