ab80fe0b0f0237cc168a415f824558ec1a65ec38
Mahmud <mahmud.linked@gmail.com>
2018-10-25 10:46:05 +0600

    After fixing pagination

--- index.php
+++ index.php
@@ -1,126 +1,128 @@
 <?php get_header(); ?>
 
-		
-        <div class="home_hero_area">
-            <div class="custom_container">
-                <div class="row align-items-center">
+        
+        <div class="home_hero_area hero_area media_hero_area">
+            <div class="container ">
+                <div class="featured_article">
 
-                    <div class="col-md-6 pad_0_10 m_m_b_15">
-                        <div class="text_section">
-                            <h2 class="blue_text_with_yellow_bg black_900">Transforming</h2>
-                            <h2 class="blue_text_with_yellow_bg black_900">unwanted uniforms </h2>
-                            <h2 class="blue_text_with_yellow_bg black_900">into clothes for </h2>
-                            <h2 class="blue_text_with_yellow_bg black_900">children in need.</h2>
-                            <p class="muse_font m_t_25">Through uniforms 4 Kids we are closing the gap between those serving in uniform, our communities and children. The program sees the uniforms continue to protect and serve. </p>
-                            <a href="#" class="yellow_box_shadow hvr-fade fw_700">Donate Now</a>
+
+                <?php
+                global $post;
+                $args = array( 'posts_per_page' => -1, 'post_type'=> 'post','category_name' => 'Featured');
+                $myposts = get_posts( $args );
+                foreach( $myposts as $post ) : setup_postdata($post); ?>
+                
+
+
+
+                     <div class="row align-items-center ">
+
+                        <div class="col-md-6 m_m_b_15">
+                            <div class="text_section">
+                                <!-- 
+                                <h2 class="red_text_yellow_bg black_900 fz_40 lh_25">Hero news</h2>
+                                <h2 class="red_text_yellow_bg black_900 fz_40 lh_25">articles go in  </h2>
+                                <h2 class="red_text_yellow_bg black_900 fz_40 lh_25">this main  </h2>
+                                <h2 class="red_text_yellow_bg black_900 fz_40 lh_25">section up here.</h2>
+                                -->
+                                <?php the_field('featured_post_heading'); ?>
+                                <p class="muse_font m_t_25"><?php the_field('featured_post_short_description'); ?></p>
+                                <a href="<?php the_permalink(); ?>" class="yellow_box_shadow hvr_fade_red fw_700 fz_20">Read more</a>
+                            </div>
                         </div>
+
+
+                        <div class="col-md-6 no_padding_on_mobile">
+                            <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/media_page.png" alt=""> -->
+                            <?php the_post_thumbnail('featured_post_image'); ?>
+                        </div>
+
                     </div>
 
 
-                    <div class="col-md-6">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt="">
-                    </div>
+
+                <?php endforeach; ?>
+                <?php wp_reset_postdata(); ?>
+
+
+
+
+
+
                 </div>
+
             </div>
         </div>      
 
-       
+ 
+        <div class="article_list_area">
+                 <div class="container">
+                     <div class="row">
 
-        <div class="blog_area featured_blog_area">
+
+
+<?php
+
+   $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+    $args = array('post_type' => 'post', 'posts_per_page' => 12, 'paged' => $paged, 'category__not_in' => '3');
+    $blog_posts = new WP_Query($args);
+    if($blog_posts->have_posts()) : while($blog_posts->have_posts()) : $blog_posts->the_post();
+?>
+
+                         <div class="col-md-4 single_article">
+                             
+                            <!-- <img src="<?php echo get_template_directory_uri(); ?>/img/article_thumb.png" alt=""> -->
+                            <?php the_post_thumbnail('post_thumb_image'); ?>
+                            <h3 class="title fz_25 fw_700"><?php the_title(); ?></h3>
+                            <?php the_excerpt(); ?>
+
+                            <a href="<?php the_permalink(); ?>" class="read_more" ><i class="fas fa-long-arrow-alt-right"></i> Read more</a>
+
+                         </div>  
+
+
+<?php endwhile;  ?>
+
+
+<?php endif;  ?>
+
+<?php wp_reset_query();  ?>
+
+
+
+                         <div class="col-md-12">
+                             <div class="view_more"><?php echo get_next_posts_link( '<i class="fas fa-long-arrow-alt-right"></i> View more' ); ?> </div>
+                             <!-- <a href="#" class="view_more">View more <i class="fas fa-long-arrow-alt-right"></i></a> -->
+                         </div>
+
+
+
+                     </div>
+                 </div>
+             </div> 
+
+
+
+
+
+        <div class="get_involved_area">
             <div class="container">
                 <div class="row">
-
-                    
-                    <div class="col-md-4">
-                        
-                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
-                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
-                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
-                        <p class="muse_font">provided to children in need</p>
-                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
-                    </div>
-                    
-                    <div class="col-md-4">
-                        
-                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
-                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
-                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
-                        <p class="muse_font">provided to children in need</p>
-                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
-                    </div>
-                    
-                    <div class="col-md-4">
-                        
-                        <h2 class="white_text_red_bg fw_900">Over</h2><br>
-                        <h2 class="white_text_red_bg fz_40 fw_900">25,000</h2> <br>
-                        <h2 class="white_text_red_bg fw_900">cothes</h2><br>
-                        <p class="muse_font">provided to children in need</p>
-                        <a href="#" class="white_box_shadow hvr_fade_red fw_700">Learn More</a>
-                    </div>
-
-                    
+                    <div class="col-md-12 footer_hero">
+                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Donate Now</h2>
+                        <p class="muse_font fz_25">Are you passionate about our cause and want to get involved? Contact us to see what ways you can volunteer and help change children's lives. </p>
+                        <a href="#" class="white_box_shadow hvr_fade_yellow fw_700 fz_25 ">Contact Us</a>
 
 
 
-                </div>
-            </div>
-        </div>
-
-        <div class="testimonial_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Why we do it</h2>
-
-                        <div class="slider">
-
-                            <div>
-
-                                <div class="text_holder ">
-                                    <p class="testimonial_text">Since getting this new clothes, I no longer worry about getting bullied at school. I feel like I can finally fit in!</p>
-                                    <p class="testi_client_name">Anthony, 7 years old,Capalaba Queensland</p>
-
-                                    <div class="testimonial_image_holder"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cropped.png" alt=""></div>
-
-                                </div>
-
-                            </div>
-
-                            <div>
-
-                                <div class="text_holder">
-                                    <p class="testimonial_text">Since getting this new clothes, I no longer worry about getting bullied at school. I feel like I can finally fit in!</p>
-                                    <p class="testi_client_name">Anthony, 7 years old,Capalaba Queensland</p>
-
-                                    <div class="testimonial_image_holder"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cropped.png" alt=""></div>
-
-                                </div>
-
-                            </div>
-
-
-                        </div>
 
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="email_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-12 col-lg-4">
-                        <h2 class="white_text_yellow_bg fw_900 m_b_50 fz_45 lh_35">Stay up to date</h2>
-                    </div>
-                    <div class="col-sm-12 col-md-12 col-lg-8">
-                        <form action="">
-                            <input class="email" type="email" placeholder="Email Address">
-                            <input type="submit" value="Subscribe" class="white_box_shadow hvr_fade_yellow fw_700 fz_25 ">
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+
 
 
 
