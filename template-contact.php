647330d8fcda570e88cca31b8c7cc21eb94d5479
Mahmud <mahmud.linked@gmail.com>
2018-10-25 12:23:31 +0600

    After contact page

--- template-contact.php
+++ template-contact.php
@@ -13,9 +13,14 @@ get_header(); ?>
                     <div class="col-md-6 m_m_b_15">
                         <div class="text_section">
                             <h2 class="blue_text_with_yellow_bg black_900">Contact Us</h2>
+                            <?php the_field('left_side_text'); ?>
+
+                                                       
                             
-                            <p class="muse_font">For all general inquires email </p>
-                            <p class="muse_font"><a href="mailto:info@uniforms4kids.org.au">info@uniforms4kids.org.au</a> </p>
+                            
+<!--
+                            <p class="muse_font mt_0">For all general inquires email </p>
+                            <p class="muse_font mt_0"><a href="mailto:info@uniforms4kids.org.au">info@uniforms4kids.org.au</a> </p>
                             
                             <div class="address_box">
                                 <p class="name fw_700">Anne Macdonald APM</p>
@@ -32,12 +37,19 @@ get_header(); ?>
                                 <p class="muse_font"><a href="mailto:pauline@uniforms4kids.com.au">E: pauline@uniforms4kids.com.au</a></p>
 
                             </div>
+-->
+
+
                         </div>
                     </div>
 
 
                     <div class="col-md-6 no_padding_on_mobile">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt="">
+                        <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/school_boy.png" alt=""> -->
+                        <?php 
+                                
+                                echo wp_get_attachment_image( get_field('right_side_image')['id'], 'full');
+                        ?> 
                     </div>
                 </div>
             </div>
